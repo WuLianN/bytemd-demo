c5969e620e4b8b4e88324ccda0e1dd0dd0962b53
WuLianN <610575024@qq.com>
2023-12-07 12:48:13 +0800

    优化 gh-pages

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -7,6 +7,8 @@
 import Editor from '@/components/Editor.vue'
 import { useRoute } from 'vue-router'
 import { ref } from 'vue'
+// @ts-ignore
+import { examples } from '@/examples/index.js'
 
 const content = ref('')
 const uploadUrl = 'http://localhost:8000/upload/file'
@@ -22,14 +24,15 @@ function contentChange(value: any) {
 }
 
 function getExampleMd() {
-  fetch(import.meta.env.VITE_APP_BASE_API + '/src/examples/examples.md').then(res => res.text()).then(md => {
-    content.value = md
-  })
+  // fetch(import.meta.env.VITE_APP_BASE_API + '/src/examples/examples.md').then(res => res.text()).then(md => {
+  //   content.value = md
+  // })
+  content.value = examples
 }
 </script>
 
 <style scoped>
 :deep(.bytemd) {
-  height: calc(100vh - 50px) !important;
+  height: calc(100vh - 10px) !important;
 }
 </style>
